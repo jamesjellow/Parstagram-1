36d746ba50eefdc0a70bbe35b8be9dbda66b577c
Monali <monalimirel@gmail.com>
2019-03-17 22:22:33 -0700

    working on comments feature

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -15,6 +15,8 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         // Do any additional setup after loading the view.
     }
     
